1498cf617e6b0c86a4edc468d0ad5d2010ef1386
KIRA <113153767+GamiKira@users.noreply.github.com>
2023-03-29 14:47:18 -0600

    Update Tarea.cpp

--- Tarea.cpp
+++ Tarea.cpp
@@ -3,34 +3,34 @@
 
 void drawTriangle()
 {
-	glBegin(GL_TRIANGLES); // comienza a dibujar el tri·ngulo
-	glVertex2f(0.0, 0.5); // vÈrtice superior
-	glVertex2f(-0.5, -0.5); // vÈrtice inferior izquierdo
-	glVertex2f(0.5, -0.5); // vÈrtice inferior derecho
-	glEnd(); // termina de dibujar el tri·ngulo
+	glBegin(GL_TRIANGLES); // comienza a dibujar el tri√°ngulo
+	glVertex2f(0.0, 0.5); // v√©rtice superior
+	glVertex2f(-0.5, -0.5); // v√©rtice inferior izquierdo
+	glVertex2f(0.5, -0.5); // v√©rtice inferior derecho
+	glEnd(); // termina de dibujar el tri√°ngulo
 }
 
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT); // limpia la pantalla
-	glColor3f(1.0, 0.0, 0.0); // establece el color rojo para el tri·ngulo
+	glColor3f(1.0, 0.0, 0.0); // establece el color rojo para el tri√°ngulo
 	glLoadIdentity(); //
-	//glRotatef(45.0, 0.0, 0.0, 1.0); // rota el objeto 45 grados alrededor del eje z
+	glRotatef(45.0, 0.0, 0.0, 1.0); // rota el objeto 45 grados alrededor del eje z
 	glScalef(2.0, 1.0, 1.0); // escala el objeto 2 veces en el eje x y 1 vez en el eje y
-	//glTranslatef(0.5, 0.5, 0.0); // traslada el objeto 0.5 en el eje x y 0.5 en el eje y
-	drawTriangle(); // dibuja el tri·ngulo
-	glFlush(); // envÌa los comandos de dibujo a la pantalla
+	glTranslatef(0.5, 0.5, 0.0); // traslada el objeto 0.5 en el eje x y 0.5 en el eje y
+	drawTriangle(); // dibuja el tri√°ngulo
+	glFlush(); // env√≠a los comandos de dibujo a la pantalla
 }
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv); // inicializa GLUT
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // establece el modo de visualizaciÛn
-	glutInitWindowSize(600, 600); // establece el tamaÒo de la ventana
-	glutCreateWindow("PolÌgonos"); // crea la ventana con un
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // establece el modo de visualizaci√≥n
+	glutInitWindowSize(600, 600); // establece el tama√±o de la ventana
+	glutCreateWindow("Pol√≠gonos"); // crea la ventana con un
 	glClearColor(1.0, 1.0, 1.0, 1.0); // establece el color de fondo de la ventana
 	gluOrtho2D(-1.0, 1.0, -1.0, 1.0);// establece el sistema de coordenadas
-	glutDisplayFunc(display); // establece la funciÛn de visualizaciÛn
+	glutDisplayFunc(display); // establece la funci√≥n de visualizaci√≥n
 	glutMainLoop(); // bucle principal de GLUT
 
 	return 0;
